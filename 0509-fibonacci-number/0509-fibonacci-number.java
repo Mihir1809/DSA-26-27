6c7ecd8039c6c0ebebb32d13b5a1d5af3ee2a863
Mihir1809 <rdixit444@gmail.com>
2026-06-23 13:49:26 +0530

    Time: 0 ms (100%), Space: 40.3 MB (99.99%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+    int first=0;
+    int second=1;
+    int third=0;
+    if(n==0||n==1)
+    return n ;
+    else
+    for(int i=2;i<=n;i++)
+    {
+    third=first +second ;
+    first=second ;
+    second =third;
+    }
+    return third;
+
+    }
+}
